efa39165a6641c2f4dbe745536e87f67ee8013d0
Iqbal-Maulana67 <fikri.rid@gmail.com>
2023-06-10 10:13:31 +0700

    Menambahkan fitur pembayaran

--- app/src/main/java/com/example/project_semester_4/Pembayaran.java
+++ app/src/main/java/com/example/project_semester_4/Pembayaran.java
@@ -3,12 +3,20 @@ package com.example.project_semester_4;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -16,14 +24,39 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Pembayaran extends AppCompatActivity {
 
     ImageView imgGallery;
-    TextView Judul;
-    private String judul;
+    TextView Judul, Harga;
+    private String judul, id_tagihan;
+
+    private Bitmap bitmap;
+    private String filePath;
+    private Uri picUri;
+
 
     private final int GALLERY_REQ_CODE = 1000;
     @Override
@@ -31,12 +64,7 @@ public class Pembayaran extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pembayaran);
 
-        //judul
-        Judul = (TextView) findViewById(R.id.Judul);
-
-        Bundle extras = getIntent().getExtras();
-        judul = extras.getString("nama_pembayaran");
-        Judul.setText(judul);
+        setData();
 
         // Set the status bar to transparent
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -61,6 +89,7 @@ public class Pembayaran extends AppCompatActivity {
 
         imgGallery = findViewById(R.id.imgGallery);
         Button btnGallery = findViewById(R.id.btn_Camera);
+        Button kirimBtn = findViewById(R.id.kirim_Button);
 
         btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,6 +100,63 @@ public class Pembayaran extends AppCompatActivity {
                 startActivityForResult(iGallery, GALLERY_REQ_CODE);
             }
         });
+
+        kirimBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (picUri != null) {
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), picUri);
+                        uploadBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(
+                            getApplicationContext(),"Masukkan bukti terlebih dahulu",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    void setData(){
+        Judul = (TextView) findViewById(R.id.Judul);
+        Harga = findViewById(R.id.Harga);
+
+        Bundle extras = getIntent().getExtras();
+        judul = extras.getString("nama_tagihan");
+        id_tagihan = extras.getString("id_tagihan");
+
+        Judul.setText(judul);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        String url = api.api_tagihan + "tagihan/" + id_tagihan + "/detail";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            JSONObject jsonData = jsonResponse.getJSONObject("data");
+                            Harga.setText(convertToCurrency(Double.parseDouble(jsonData.getString("harga_tagihan"))));
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("ErrorResponse", error.getMessage());
+                    }
+                });
+        requestQueue.add(stringRequest);
     }
 
     @Override
@@ -80,6 +166,7 @@ public class Pembayaran extends AppCompatActivity {
         if (resultCode==RESULT_OK){
             if (requestCode==GALLERY_REQ_CODE){
                 imgGallery.setImageURI(data.getData());
+                picUri = data.getData();
             }
         }
     }
@@ -96,4 +183,103 @@ public class Pembayaran extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public String getPath(Uri uri) {
+        try {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+            String document_id = cursor.getString(0);
+            Log.d("documentID" , cursor.moveToFirst() +  " tes");
+            document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+            cursor.close();
+
+            cursor = getContentResolver().query(
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+            cursor.moveToFirst();
+            String path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
+            cursor.close();
+
+            return "test";
+        } catch(Exception error){
+            Log.d("errorPath", error.getMessage());
+            return "false";
+        }
+
+    }
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    private void uploadBitmap(final Bitmap bitmap) {
+
+        String url = api.api_validasi;
+        Bundle extras = getIntent().getExtras();
+        SharedPreferences sharedPreferences = getSharedPreferences("siswaPreferences", Context.MODE_PRIVATE);
+        String nisn = sharedPreferences.getString("nisn", "");
+        id_tagihan = extras.getString("id_tagihan");
+
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, url,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        try {
+                            JSONObject obj = new JSONObject(new String(response.data));
+                            if (obj.getString("code").equals("200")){
+                                Intent i = new Intent(getApplicationContext(), dashboard.class);
+                                startActivity(i);
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(getApplicationContext(), "Permintaan gagal! Coba beberapa saat lagi.", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                        Log.e("GotError",""+error.getMessage());
+                    }
+                }) {
+
+
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("nisn", nisn);
+                params.put("id_tagihan", id_tagihan);
+
+                return params;
+            }
+
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                long imagename = System.currentTimeMillis();
+                params.put("gambar_pembayaran", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
+                return params;
+            }
+        };
+
+        //adding the request to volley
+        Volley.newRequestQueue(this).add(volleyMultipartRequest);
+    }
+
+    public String convertToCurrency(double number) {
+        // Create a NumberFormat instance with the desired locale and currency style
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+
+        // Convert the number to currency format
+        String currencyString = currencyFormat.format(number);
+
+        // Return the currency string
+        return currencyString;
+    }
 }
--- app/src/main/java/com/example/project_semester_4/dashboard.java
+++ app/src/main/java/com/example/project_semester_4/dashboard.java
@@ -6,11 +6,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,6 +24,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import  com.android.volley.Request;
@@ -34,6 +38,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 
@@ -49,16 +54,23 @@ public class dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        TextView titleDashboard = findViewById(R.id.title_dashboard);
+        TextView txtTotal = findViewById(R.id.total_tagihan);
 
+        SharedPreferences sharedPreferences = getSharedPreferences("siswaPreferences", Context.MODE_PRIVATE);
+        Log.d("NISNShared", sharedPreferences.getString("nisn", ""));
+        if (sharedPreferences.getString("nisn", "").equals("")){
+            startActivity(new Intent(getApplicationContext(), login.class));
+        }
 
-        // Set the status bar to transparent
+        titleDashboard.setText("Hai " + sharedPreferences.getString("nama", "") +  "!");
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.BLACK);
         }
 
-        //toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
@@ -67,7 +79,7 @@ public class dashboard extends AppCompatActivity {
         autoCompleteTextView = findViewById(R.id.AutoCompleteTextview);
 
         //array daftar bulan
-        String[] Subjects = new String[]{"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
+        String[] Subjects = new String[]{"Semua" ,"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
 
         //membuat adapter array dan berikan parameter yang diperlukan
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown_dashboard, Subjects);
@@ -78,68 +90,139 @@ public class dashboard extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getApplicationContext(), "" + autoCompleteTextView.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+                adapterTagihan = new adapter_tagihan(tagihanArrayList);
+                String url = api.api_tagihan + sharedPreferences.getString("nisn", "") + "/" + autoCompleteTextView.getText();
+
+                adapterTagihan.clearData();
+                recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+                adapterTagihan = new adapter_tagihan(tagihanArrayList);
+
+                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(dashboard.this);
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setAdapter(adapterTagihan);
+                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject jsonResponse = new JSONObject(response);
+                                    txtTotal.setText(convertToCurrency(Double.parseDouble(jsonResponse.getString("total"))));
+                                    JSONArray jsonArray = jsonResponse.getJSONArray("data");
+                                    tagihanArrayList = new ArrayList<>();
+                                    for(int i=0; i<jsonArray.length(); i++){
+                                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                        JSONObject jsonObject1 = jsonObject.getJSONObject("list_jenis_tagihan");
+                                        String textTitle = "";
+                                        String id_tagihan = jsonObject.getString("id_tagihan");
+                                        if (jsonObject.getString("bulan").equals("null")){
+                                            textTitle = jsonObject1.getString("nama_jenis_tagihan");
+                                        }else{
+                                            textTitle = jsonObject1.getString("nama_jenis_tagihan") + " " + jsonObject.getString("bulan") +  " " + jsonObject.getString("tahun");
+                                        }
+
+                                        tagihan murid = new tagihan(textTitle, id_tagihan);
+                                        tagihanArrayList.add(murid);
+
+                                        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+                                        adapterTagihan = new adapter_tagihan(tagihanArrayList);
+
+                                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(dashboard.this);
+                                        recyclerView.setLayoutManager(layoutManager);
+                                        recyclerView.setAdapter(adapterTagihan);
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener(){
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.e("ErrorResponse", error.getMessage());
+                            }
+                        });
+                requestQueue.add(stringRequest);
             }
         });
-        //batas akhir dropdown
 
-        //recycleview
-        String jsonData = "[{'name':'Pembayaran 1'}," + "{'name': 'Pembayaran 2'}," + "{'name': 'Pembayaran 3'}," + "{'name': 'Pembayaran 4'}," + "{'name': 'Pembayaran 5'}," +
-                "{'name': 'Pembayaran 6'}," + "{'name': 'Pembayaran 7'}," + "{'name': 'Pembayaran 8'}," + "{'name': 'Pembayaran 9'}," + "{'name': 'Pembayaran 10'}," + "{'name': 'Pembayaran 11'}," +
-                "{'name': 'Pembayaran 12'}," + "{'name': 'Pembayaran 13'}," + "{'name': 'Pembayaran 14'}," + "{'name': 'Pembayaran 15'}," + "{'name': 'Pembayaran 16'}]";
+        setData();
+    }
 
-        setData(jsonData);
+    public String convertToCurrency(double number) {
+        // Create a NumberFormat instance with the desired locale and currency style
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        adapterTagihan = new adapter_tagihan(tagihanArrayList);
+        // Convert the number to currency format
+        String currencyString = currencyFormat.format(number);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(dashboard.this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapterTagihan);
+        // Return the currency string
+        return currencyString;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.toolbar_menu, menu);
-        return  true;
+        return true;
     }
 
-    void setData(String jsonString){
+    void setData(){
+        SharedPreferences sharedPreferences = getSharedPreferences("siswaPreferences", Context.MODE_PRIVATE);
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-        String url = "https://0942-103-109-209-244.ngrok-free.app/api/api_tagihan";
+        String url = api.api_tagihan + sharedPreferences.getString("nisn", "");
+        TextView txtTotal = findViewById(R.id.total_tagihan);
+
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            txtTotal.setText(convertToCurrency(Double.parseDouble(jsonResponse.getString("total"))));
+                            JSONArray jsonArray = jsonResponse.getJSONArray("data");
+                            tagihanArrayList = new ArrayList<>();
+                            for(int i=0; i<jsonArray.length(); i++){
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                JSONObject jsonObject1 = jsonObject.getJSONObject("list_jenis_tagihan");
+                                String textTitle = "";
+                                String id_tagihan = jsonObject.getString("id_tagihan");
+                                if (jsonObject.getString("bulan").equals("null")){
+                                    textTitle = jsonObject1.getString("nama_jenis_tagihan");
+                                }else{
+                                    textTitle = jsonObject1.getString("nama_jenis_tagihan") + " " + jsonObject.getString("bulan") +  " " + jsonObject.getString("tahun");
+                                }
+                                tagihan murid = new tagihan(textTitle, id_tagihan);
+                                tagihanArrayList.add(murid);
 
+                                recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+                                adapterTagihan = new adapter_tagihan(tagihanArrayList);
+
+                                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(dashboard.this);
+                                recyclerView.setLayoutManager(layoutManager);
+                                recyclerView.setAdapter(adapterTagihan);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 },
                 new Response.ErrorListener(){
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
+                        Log.e("ErrorResponse", error.getMessage());
                     }
                 });
-
-
-        try {
-            JSONArray jsonArray = new JSONArray(jsonString);
-            tagihanArrayList = new ArrayList<>();
-            for(int i=0; i<jsonArray.length(); i++){
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                tagihan murid = new tagihan(jsonObject.getString("name"));
-
-                tagihanArrayList.add(murid);
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
+        requestQueue.add(stringRequest);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        SharedPreferences sharedPreferences = getSharedPreferences("siswaPreferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         int id = item.getItemId();
 
         if (id == R.id.edit_password){
@@ -148,8 +231,9 @@ public class dashboard extends AppCompatActivity {
             return true;
         }
         if (id == R.id.exit){
-            // Menghapus Status login dan kembali ke Login Activity
-            onBackPressed();
+            editor.remove("nisn");
+            editor.apply();
+            startActivity(new Intent(getApplicationContext(), login.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
